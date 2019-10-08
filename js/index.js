1cde2f34fe08c41613c114824579c33c7e2d6376
mateoepalza <mateoepalzaramirez@gmail.com>
2019-10-08 00:06:28 -0500

    i added the responsive design

--- js/index.js
+++ js/index.js
@@ -6,20 +6,29 @@ document.addEventListener("DOMContentLoaded", function(){
   var res;
   document.getElementById("boton").addEventListener("click", function(){
 
-    var rMinu = document.getElementById("radioMe").value;
-    rMayus = document.getElementById("radioMa").value;
-    q = document.getElementById("carga").value;
-    q = q/1000000000;
-
-    if(rMinu < rMayus){
-      var res = (k*q)/rMayus;
-      document.getElementById("image-container").innerHTML = "<img id='imgP' src='img/rMenor.png' >";
+    if(document.getElementById('radioMe').value == '' || document.getElementById('radioMa').value == '' || document.getElementById('carga').value == ''){
+      document.getElementById('alert').style.top = "0px";
+      setTimeout(function(){
+        document.getElementById('alert').style.top = "-100px";
+      }, 5000);
     }else{
-      var res = (k*q)/rMinu;
-      document.getElementById("image-container").innerHTML ="<img id='imgP' src='img/rMayor.png' >";
-    }
 
-    document.getElementById("resultado").innerHTML="El potencial eléctrico es de : "+res+" J";
+      var rMinu = document.getElementById("radioMe").value;
+      rMayus = document.getElementById("radioMa").value;
+      q = document.getElementById("carga").value;
+      q = q/1000000000;
+
+      if(rMinu < rMayus){
+        var res = (k*q)/rMayus;
+        document.getElementById("image-container").innerHTML = "<img id='imgP' src='img/rMenor.png' >";
+      }else{
+        var res = (k*q)/rMinu;
+        document.getElementById("image-container").innerHTML ="<img id='imgP' src='img/rMayor.png' >";
+      }
+
+      document.getElementById("resultado").innerHTML="El potencial eléctrico es de : "+res+" Voltios";
+
+    }
 
   });
 
